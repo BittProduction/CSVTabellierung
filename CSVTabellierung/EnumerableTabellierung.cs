cc8ddd0a8ac75b33a1a8cb87004c229d51867cec
Markus Bittner <sepH2807@gmail.com>
2021-01-30 15:21:18 +0100

    Projektdateien hinzufügen.

---
+++ CSVTabellierung/EnumerableTabellierung.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EnumerableTabellierung
+{
+    public class EnumerableTabellierung
+    {
+        private Char CSVSeperator { get; set; }
+        private bool IsFirstLineHeadline { get; set; }
+
+        public EnumerableTabellierung(Char Seperator = ';', bool IsFirstLineHeadline = true)
+        {
+            this.CSVSeperator = Seperator;
+            this.IsFirstLineHeadline = IsFirstLineHeadline;
+        }
+
+        public IEnumerable<IEnumerable<string>> Split(List<string> lists)
+        {
+            List<List<string>> result = new List<List<string>>();
+
+            foreach (var line in lists)
+            {
+                var splitted = new List<string>();
+                line.Split(this.CSVSeperator).ToList().ForEach(item =>
+                {
+                    splitted.Add(item);
+                });
+                result.Add(splitted);
+            }
+
+            return result;
+        }
+
+        public IEnumerable<IEnumerable<int>> GetColumnLengths(IEnumerable<IEnumerable<string>> List)
+        {
+            List<List<int>> result = new List<List<int>>();
+
+            foreach (var column in List)
+            {
+                List<int> ColumLength = new List<int>();
+                foreach (var columnItem in column)
+                {
+                    ColumLength.Add(columnItem.Length);
+                }
+                result.Add(ColumLength);
+            }
+
+            return result;
+        }
+
+        public IEnumerable<int> GetMaxColumnLenghtsPerRow(IEnumerable<IEnumerable<int>> source)
+        {
+            List<int> result = new List<int>();
+
+            for (int row = 0; row < source.Count(); row++)
+            {
+                for (int i = 0; i < source.ElementAt(row).Count(); i++)
+                {
+                    if (row == 0)
+                    {
+                        result.Add(source.ElementAt(row).ElementAt(i)); //First Row. Just take this Value for MaxLength
+                    }
+                    else
+                    {
+                        if (result.ElementAt(i) < source.ElementAt(row).ElementAt(i)) //Check if the maxLength we already added at Columnindex is smaller tahn the actual value
+                        {
+                            //Replace it with the current one
+                            result[i] = source.ElementAt(row).ElementAt(i);
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public IEnumerable<string> Convert(IEnumerable<string> Data)
+        {
+            List<string> result = new List<string>();
+
+            var splitted = Split(Data.ToList());
+
+            var MaxColumLengths = GetMaxColumnLenghtsPerRow(GetColumnLengths(splitted));
+
+            for (var row = 0; row < splitted.Count(); row++)
+            {
+                string output = string.Empty;
+                for (int column = 0; column < splitted.ElementAt(row).Count(); column++)
+                {
+                    output = String.Join("|", output, splitted.ElementAt(row).ElementAt(column).PadRight(MaxColumLengths.ElementAt(column)));
+                }
+                result.Add(output);
+                if (row == 0)
+                {
+                    if (IsFirstLineHeadline == true)
+                    {
+                        string seperator = string.Empty;
+                        //Add Header seperator
+                        for (int col = 0; col < MaxColumLengths.Count(); col++)
+                        {
+                            seperator = String.Join("|", seperator, new String('-', MaxColumLengths.ElementAt(col)));
+                        }
+                        result.Add(seperator);
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ CSVTabellierungTester/Tester.cs
@@ -0,0 +1,112 @@
+using System;
+using Xunit;
+using EnumerableTabellierung;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSVTabellierungTester
+{
+    public class Tester
+    {
+        [Fact]
+        public void TestSplitting()
+        {
+            var service = new EnumerableTabellierung.EnumerableTabellierung();
+
+            var result = service.Split(new List<string>() { "Name;Strasse;Ort;Alter" }).ToList();
+
+            Assert.True(result.Count() != 0);
+
+        }
+
+        [Fact]
+        public void TestSplitResult()
+        {
+            List<List<string>> expected = new List<List<string>>();
+            expected.Add(new List<string>() { "Name", "Strasse", "Ort", "Alter" });
+
+            var service = new EnumerableTabellierung.EnumerableTabellierung();
+
+            var result = service.Split(new List<string>() { "Name;Strasse;Ort;Alter" }).ToList();
+
+            Assert.True(Enumerable.SequenceEqual(expected[0],result[0]));
+        }
+
+        [Fact]
+        public void TestCoulmnLength()
+        {
+            List<List<int>> expected = new List<List<int>>();
+            expected.Add(new List<int> (){ 4, 7, 3, 5 });
+
+            var service = new EnumerableTabellierung.EnumerableTabellierung();
+
+            var splitted = service.Split(new List<string>() { "Name;Strasse;Ort;Alter" }).ToList();
+
+            var result = service.GetColumnLengths(splitted).ToList();
+
+            Assert.True(Enumerable.SequenceEqual(expected[0], result[0]));
+        }
+
+        [Fact]
+        public void TestGetMaxColumnLengthPerRow()
+        {
+            List<string> source = new List<string>();
+            source.Add("Name;Strasse;Ort;Alter");                       //4 , 7, 3,5
+            source.Add("Peter Pan;Am Hang 5;12345 Einsam;42");          //9 , 9,12,2
+            source.Add("Maria Schmitz;Kölner Straße 45;50123 Köln;43"); //13,16,10,2
+
+            List<int> expected = new List<int>() {13,16, 12,5};
+
+            var service = new EnumerableTabellierung.EnumerableTabellierung();
+
+            var splitted = service.Split(source.ToList());
+
+            var ColumnLengths = service.GetColumnLengths(splitted).ToList();
+
+            var result = service.GetMaxColumnLenghtsPerRow(ColumnLengths);
+
+            Assert.True(Enumerable.SequenceEqual(expected, result));
+        }
+
+        [Fact]
+        public void TestConsoleOutputWithHeadline()
+        {
+            List<string> source = new List<string>();
+            source.Add("Name;Strasse;Ort;Alter");                       //4 , 7, 3,5
+            source.Add("Peter Pan;Am Hang 5;12345 Einsam;42");          //9 , 9,12,2
+            source.Add("Maria Schmitz;Kölner Straße 45;50123 Köln;43"); //13,16,10,2
+
+            List<string> expected = new List<string>();
+            expected.Add("|Name         |Strasse         |Ort         |Alter");
+            expected.Add("|-------------|----------------|------------|-----");
+            expected.Add("|Peter Pan    |Am Hang 5       |12345 Einsam|42   ");
+            expected.Add("|Maria Schmitz|Kölner Straße 45|50123 Köln  |43   ");
+
+            var service = new EnumerableTabellierung.EnumerableTabellierung();
+
+            var result = service.Convert(source);
+
+            Assert.True(Enumerable.SequenceEqual(expected, result));
+        }
+
+        [Fact]
+        public void TestConsoleOutputWithoutHeadline()
+        {
+            List<string> source = new List<string>();
+            source.Add("Name;Strasse;Ort;Alter");                       //4 , 7, 3,5
+            source.Add("Peter Pan;Am Hang 5;12345 Einsam;42");          //9 , 9,12,2
+            source.Add("Maria Schmitz;Kölner Straße 45;50123 Köln;43"); //13,16,10,2
+
+            List<string> expected = new List<string>();
+            expected.Add("|Name         |Strasse         |Ort         |Alter");
+            expected.Add("|Peter Pan    |Am Hang 5       |12345 Einsam|42   ");
+            expected.Add("|Maria Schmitz|Kölner Straße 45|50123 Köln  |43   ");
+
+            var service = new EnumerableTabellierung.EnumerableTabellierung(IsFirstLineHeadline: false);
+
+            var result = service.Convert(source);
+
+            Assert.True(Enumerable.SequenceEqual(expected, result));
+        }
+    }
+}
